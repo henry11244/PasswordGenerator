66f1ade85cf18585c3c662f3d594f0224d4e02be
Henry Nguyen <hln11244@gmail.com>
2022-06-23 16:36:55 -0700

    generator operational

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 // Random number function 
-var random = () => Math.floor(Math.random() * Variable)
+var random = (Variable) => Math.floor(Math.random() * Variable)
 
 // Number array
 var NumberArray = Array.from(Array(10).keys())
@@ -16,10 +16,14 @@ const alphabetLower = alphabetCap.map(element => { return element.toLowerCase() 
 // Special character array
 var SpecialCharacter = []
 
-var passwordLength = 0
+var passwordLength = 8
 
-// Check which special characters are selected, which is used when button is pressed
-function specialCharacterCheck() {
+// arrays of arrays for use based on which perameters are checked
+var PasswordArray = []
+
+// Check which special characters, numbers and Capitlized/Lowercased letters are selected, 
+// which is used when button is pressed
+function checkBoxes() {
     if (document.querySelector('#exclamationMark').checked) { SpecialCharacter.push("!") }
     if (document.querySelector('#atSymbol').checked) { SpecialCharacter.push("@") }
     if (document.querySelector('#questionMark').checked) { SpecialCharacter.push("?") }
@@ -27,8 +31,14 @@ function specialCharacterCheck() {
     if (document.querySelector('#dollarSign').checked) { SpecialCharacter.push("$") }
     if (document.querySelector('#percent').checked) { SpecialCharacter.push("%") }
     if (document.querySelector('#andSign').checked) { SpecialCharacter.push("&") }
+    if (SpecialCharacter.length !== 0) { PasswordArray.push(SpecialCharacter) };
+    if (document.querySelector('#CapitalBox').checked) { PasswordArray.push(alphabetCap) }
+    if (document.querySelector('#LowercaseBox').checked) { PasswordArray.push(alphabetLower) }
+    if (document.querySelector('#Numbers').checked) { PasswordArray.push(NumberArray) }
 }
 
+// var LowercaseBox = document.querySelector('#LowercaseBox');
+// var CapitalBox = document.querySelector('#CapitalBox')
 
 // Password length updates
 var length = document.querySelector('#length')
@@ -36,20 +46,27 @@ length.addEventListener("input", function () {
     passwordLength = length.value
 })
 
-// arrays of arrays for use based on which perameters are checked
-var PasswordArray = []
+var password = []
+var passwordBox = document.querySelector('#passwordBox')
 
 // Script to create password 
 generatePassword = document.querySelector('#generatePassword')
 generatePassword.addEventListener('click', function () {
-    specialCharacterCheck();
-    if (SpecialCharacter.length !== 0) { PasswordArray.push(SpecialCharacter) };
-    if (document.querySelector('#LowercaseBox').checked) { PasswordArray.push(alphabetLower) }
-    if (document.querySelector('#CapitalBox').checked) { PasswordArray.push(alphabetCap) }
 
-    // to reset special character array after each run
-    setInterval(function () { SpecialCharacter.splice(0, SpecialCharacter.length) }, 1000)
-    setInterval(function () { PasswordArray.splice(0, PasswordArray.length) }, 1000)
+    checkBoxes();
+    for (let i = 0; i < passwordLength; i++) {
+        var newArray = PasswordArray[random(PasswordArray.length)];
+        var newCharacter = newArray[random(newArray.length)];
+        password.push(newCharacter);
+    }
+    passwordBox.innerText = password.join('')
+
+
+
+    // to reset arrays after each run
+    setInterval(function () { SpecialCharacter.splice(0, SpecialCharacter.length) }, 100)
+    setInterval(function () { PasswordArray.splice(0, PasswordArray.length) }, 100)
+    setInterval(function () { password.splice(0, password.length) }, 100)
 })
 
 
